005259cc8c8405ff5346add03a463729c86cb8ab
SomewhatSane <thisis@somewhatsane.co.uk>
2021-08-21 20:26:08 +0100

    Removed server side code

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $latestStable = "2.2.0";
-    $latestBeta = "2.3.0";
-    $downloadUrlsStable = ["https://github.com/SomewhatSane/VPNShield-Exiled-Edition", "https://plugins.exiled.host"];
-    $downloadUrlsBeta = ["https://github.com/SomewhatSane/VPNShield-Exiled-Edition", "https://plugins.exiled.host"];
-    
-?>
